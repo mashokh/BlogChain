f683195de73d84960f93a4ad97fa607a3757e11c
lukaaduashvili <lukaaduashvili@gmail.com>
2021-08-09 12:26:33 +0400

    Update CommentsDao add CommentServlet

---
+++ src/main/java/controller/CommentServlet.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "comments", urlPatterns = { "/Blog/*"})
+
+public class CommentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doGet(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        super.doPost(httpServletRequest, httpServletResponse);
+    }
+}
--- src/main/java/model/CommentsDao.java
+++ src/main/java/model/CommentsDao.java
@@ -4,16 +4,46 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CommentsDao {
-    public static ResultSet getCommentsByBlogId(int userId) throws SQLException {
+    public static ArrayList<Comments> getCommentsByBlogId(int blogId) throws SQLException {
+        ArrayList<Comments> res = new ArrayList<>();
         Connection conn = DataBase.getConnection();
         PreparedStatement statement = conn.prepareStatement("select * from comments where blog_id = ?;");
+        statement.setInt(1, blogId);
+
+        ResultSet rs = statement.executeQuery();
+        if(!rs.next())
+            return null;
+        while(rs.next()) {
+            res.add(new Comments(rs.getString(0), rs.getString(1), rs.getString(2), rs.getString(3)));
+        }
+        return res;
+    }
+
+    public static ArrayList<Comments> getCommentsByUserId(int userId) throws SQLException {
+        ArrayList<Comments> res = new ArrayList<>();
+        Connection conn = DataBase.getConnection();
+        PreparedStatement statement = conn.prepareStatement("select * from comments where user_id = ?;");
         statement.setInt(1, userId);
 
         ResultSet rs = statement.executeQuery();
         if(!rs.next())
             return null;
-        return rs;
+        while(rs.next()) {
+            res.add(new Comments(rs.getString(0), rs.getString(1), rs.getString(2), rs.getString(3)));
+        }
+        return res;
+    }
+
+    public static void insertComment(int userId, int blogId, String date, String text) throws SQLException {
+        Connection conn = DataBase.getConnection();
+        PreparedStatement statement = conn.prepareStatement("INSERT INTO comments (blog_id, user_id, text, created_at) VALUES(?, ?, ?, ?);");
+        statement.setInt(1, blogId);
+        statement.setInt(2, userId);
+        statement.setString(3, text);
+        statement.setString(4, date);
+        statement.execute();
     }
 }

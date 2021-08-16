8868b21c8d65dea8f3676f11b68bafdc7ff63c34
masho <72522103+mashokh@users.noreply.github.com>
2021-08-16 11:13:05 +0400

    Update BlogTest.java

--- src/main/java/model/BlogTest.java
+++ src/main/java/model/BlogTest.java
@@ -71,8 +71,8 @@ public class BlogTest extends TestCase {
 
     public void testTruncatedText(){
         assertEquals(blog.getTruncatedText(), "counter strike");
-        blog.setText("cs cs cs cs cs cs cs cs cs cs cs cs");
-        assertEquals("cs cs cs cs cs cs cs cs cs cs ...", blog.getTruncatedText());
+        blog.setText("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31");
+        assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 ...", blog.getTruncatedText());
         blog.setText("counter strike");
     }
 }
